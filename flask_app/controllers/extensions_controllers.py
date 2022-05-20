823991fa1fc6b45826e6b60271a3b8813462a01c
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-19 23:59:07 -0400

    outline setup

---
+++ flask_app/controllers/extensions_controllers.py
@@ -0,0 +1,10 @@
+from flask_app import app
+from flask import redirect, render_template 
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
