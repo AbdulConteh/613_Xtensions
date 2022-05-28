84473ea1c2649f0dc4a28026c6e85c2f89a4ba68
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-27 23:59:36 -0400

    login and registration

--- flask_app/controllers/users_controllers.py
+++ flask_app/controllers/users_controllers.py
@@ -1,19 +1,44 @@
 from flask_app import app
 from flask_app.models.users_models import User
-from flask import redirect, render_template, request
+from flask_bcrypt import Bcrypt
+from flask import flash
+from flask import redirect, render_template, request, session
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
     return render_template('home.html')
 
-@app.route('/register')
+@app.route('/register', methods=['POST'])
 def register_user():
+    if not User.validate_users(request.form):
+        return redirect('/')
     data = {
         "first_name" : request.template['first_name'],
         "last_name" : request.template['last_name'],
         "email" : request.template['email'],
-        "password" : request.template['password']
+        "password" : bcrypt.generate_password_hash(request.form['password'])
     }
+    id = User.created_user(data)
+    print(id)
+    session['user_id'] = id
+    return redirect("/")
+
+@app.route("/login", methods=['POST'])
+def login():
+    data = {
+        "email" : request.form["email"]
+    }
+    user = User.get_user(data)
+    print(user)
+    if not user:
+        flash("Unregistered email. Please try again", "login")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid password.", "login")
+        return redirect('/')
+    session['user_id'] = user.id
+    return redirect ('/profile')
 
 @app.route('/contact')
 def contact():
--- flask_app/models/users_models.py
+++ flask_app/models/users_models.py
@@ -1,7 +1,7 @@
 import re 
 from flask import flash
 from flask_app.config.mysqlconnection import connectToMySQL
-db = "613xtensions_db"
+db = "613xtensions"
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
